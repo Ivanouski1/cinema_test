a14222d09927099b15022bfc2e4da57624dd560d
Andrei Ivanouski1 <Andrei_Ivanouski1@epam.com>
2023-03-07 01:34:45 +0300

    test

--- file1.sql
+++ file1.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS SimpleTable
 (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     CustomerName VARCHAR(30) NOT NULL,
-    PerfomersName VARCHAR(20) NOT NULL
+    PerfomersName VARCHAR(20) NOT NULL,
+    Adress VARCHAR(20) NOT NULL
 );
 INSERT SimpleTable(CustomerName, PerfomersName) 
-VALUES ('Andrew', 'Dima');
+VALUES ('Andrew', 'Dima', 'Minsk');
